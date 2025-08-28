e5e351ee2189f7555a82db6612ca48893a755513
gsindri <gsindrih@gmail.com>
2025-08-28 10:30:51 +0200

    refactor: update AppLayout main structure

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { SidebarProvider } from '@/components/ui/sidebar-provider'
 import { EnhancedAppSidebar } from './EnhancedAppSidebar'
@@ -13,20 +12,18 @@ interface AppLayoutProps {
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <SidebarProvider>
-        <div className="flex w-full h-screen overflow-hidden bg-background">
+      <div className="flex w-full h-screen overflow-hidden bg-background">
         <EnhancedAppSidebar />
-        
-          <div className="flex-1 flex flex-col">
-            <TopNavigation />
 
-            <main className="app-scroll flex-1 pt-[var(--header-h)]">
-              <ElevationBanner />
-              <div className="p-6">
-                {children}
-              </div>
-            </main>
+        <div className="flex-1 flex flex-col">
+          <TopNavigation />
+
+          <main className="app-scroll flex-1 pt-[var(--header-h)] w-full max-w-[1600px] 2xl:max-w-[1800px] mx-auto px-4 sm:px-6 lg:px-8">
+            <ElevationBanner />
+            {children}
+          </main>
         </div>
-        
+
         <CartDrawer />
       </div>
     </SidebarProvider>
