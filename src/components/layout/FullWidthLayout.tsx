275d5a6d61f462d84af23a0264862586a28cae58
gsindri <gsindrih@gmail.com>
2025-08-29 22:58:54 +0200

    fix: make catalog layout scrollable

--- src/components/layout/FullWidthLayout.tsx
+++ src/components/layout/FullWidthLayout.tsx
@@ -1,41 +1,35 @@
-
 import React from 'react'
 import { SidebarProvider } from '@/components/ui/sidebar-provider'
 import { EnhancedAppSidebar } from './EnhancedAppSidebar'
 import { TopNavigation } from './TopNavigation'
-import { ElevationBanner } from './ElevationBanner'
 import { CartDrawer } from '@/components/cart/CartDrawer'
 
 interface FullWidthLayoutProps {
   children: React.ReactNode
 }
 
-function FullWidthLayoutContent({ children }: FullWidthLayoutProps) {
-  return (
-    <div
-      className="min-h-screen grid bg-background"
-      style={{ gridTemplateColumns: 'var(--sidebar-width,16rem) minmax(0,1fr)' }}
-    >
-      <aside className="sticky top-0 h-screen w-[var(--sidebar-width,16rem)]">
-        <EnhancedAppSidebar />
-      </aside>
-
-      <div className="min-w-0 flex flex-col">
-        <TopNavigation />
-
-        <main className="app-scroll flex-1 pt-[var(--header-h)] transition-all duration-200 will-change-auto motion-reduce:transition-none">
-          <ElevationBanner />
-          {children}
-        </main>
-      </div>
-    </div>
-  )
-}
-
 export function FullWidthLayout({ children }: FullWidthLayoutProps) {
   return (
     <SidebarProvider>
-      <FullWidthLayoutContent>{children}</FullWidthLayoutContent>
+      <div
+        className="min-h-screen grid"
+        style={{ gridTemplateColumns: 'var(--sidebar-width,16rem) minmax(0,1fr)' }}
+      >
+        {/* Sidebar column */}
+        <aside className="sticky top-0 h-screen w-[var(--sidebar-width,16rem)]">
+          <EnhancedAppSidebar />
+        </aside>
+
+        {/* Main column */}
+        <div className="min-w-0 h-screen flex flex-col overflow-hidden">
+          <TopNavigation />
+          {/* This is the ONLY scroll container */}
+          <div className="app-scroll flex-1 overflow-y-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </div>
+      </div>
+
       <CartDrawer />
     </SidebarProvider>
   )
