aeb98e2f0068a30075900e3bf6384375a8b27bfd
gsindri <gsindrih@gmail.com>
2025-09-01 15:30:43 +0000

    refactor layout to mount top nav once

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import { SidebarProvider } from '@/components/ui/sidebar-provider'
-import { EnhancedAppSidebar } from './EnhancedAppSidebar'
-import { TopNavigation } from './TopNavigation'
-import { CartDrawer } from '@/components/cart/CartDrawer'
 import { Outlet } from 'react-router-dom'
+import { FullWidthLayout } from './FullWidthLayout'
 
 export function AppLayout() {
   return (
-    <SidebarProvider>
-      <div className="flex flex-col min-h-screen">
-        <TopNavigation />
-
-        <div className="flex flex-1 min-h-0">
-          <aside className="w-[var(--sidebar-width,16rem)]">
-            <EnhancedAppSidebar />
-          </aside>
-
-          <main className="flex-1 min-w-0 app-scroll pt-[var(--header-h)] overflow-y-auto px-[clamp(16px,2vw,28px)]">
-            <Outlet />
-          </main>
-        </div>
-
-        <CartDrawer />
-      </div>
-    </SidebarProvider>
+    <FullWidthLayout>
+      <Outlet />
+    </FullWidthLayout>
   )
 }
--- src/components/layout/CatalogLayout.tsx
+++ src/components/layout/CatalogLayout.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { SidebarProvider } from '@/components/ui/sidebar-provider'
-import { EnhancedAppSidebar } from './EnhancedAppSidebar'
-import { TopNavigation } from './TopNavigation'
-import { CartDrawer } from '@/components/cart/CartDrawer'
 import { Outlet } from 'react-router-dom'
+import { FullWidthLayout } from './FullWidthLayout'
 
 /**
  * Layout for catalog pages.
@@ -12,22 +9,8 @@ import { Outlet } from 'react-router-dom'
  */
 export function CatalogLayout() {
   return (
-    <SidebarProvider>
-      <div className="flex flex-col min-h-screen">
-        <TopNavigation />
-
-        <div className="flex flex-1 min-h-0">
-          <aside className="w-[var(--sidebar-width,16rem)]">
-            <EnhancedAppSidebar />
-          </aside>
-
-          <main className="flex-1 min-w-0 app-scroll pt-[var(--header-h)] overflow-y-auto px-[clamp(16px,2vw,28px)]">
-            <Outlet />
-          </main>
-        </div>
-
-        <CartDrawer />
-      </div>
-    </SidebarProvider>
+    <FullWidthLayout>
+      <Outlet />
+    </FullWidthLayout>
   )
 }
