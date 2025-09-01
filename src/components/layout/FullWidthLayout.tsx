9be5539ab912e2bd0461f31c5c3c1ecdc6d2422f
gsindri <gsindrih@gmail.com>
2025-09-01 14:49:05 +0000

    refactor layout to align fixed header

--- src/components/layout/FullWidthLayout.tsx
+++ src/components/layout/FullWidthLayout.tsx
@@ -13,31 +13,26 @@ interface FullWidthLayoutProps {
 export function FullWidthLayout({ children, offsetContent = true }: FullWidthLayoutProps) {
   return (
     <SidebarProvider>
-      <div
-        className="min-h-screen grid"
-        style={{ gridTemplateColumns: 'var(--sidebar-width,16rem) minmax(0,1fr)' }}
-      >
-        {/* Sidebar column */}
-        <aside className="sticky top-0 h-svh w-[var(--sidebar-width,16rem)]">
-          <EnhancedAppSidebar />
-        </aside>
+      <div className="flex flex-col min-h-screen">
+        <TopNavigation />
 
-        {/* Main column */}
-        <div className="min-w-0 h-svh flex flex-col">
-          <TopNavigation />
-          {/* The ONLY scroll container */}
+        <div className="flex flex-1 min-h-0">
+          <aside className="w-[var(--sidebar-width,16rem)]">
+            <EnhancedAppSidebar />
+          </aside>
+
           <div
             className={cn(
-              'app-scroll flex-1 min-h-0 overflow-y-auto px-4 sm:px-6 lg:px-8 xl:px-10 2xl:px-12',
+              'flex-1 min-w-0 app-scroll overflow-y-auto px-4 sm:px-6 lg:px-8 xl:px-10 2xl:px-12',
               offsetContent && 'pt-[var(--header-h)]',
             )}
           >
             {children}
           </div>
         </div>
-      </div>
 
-      <CartDrawer />
+        <CartDrawer />
+      </div>
     </SidebarProvider>
   )
 }
