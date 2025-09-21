06c2ed46e7f95243b2f9c4712e8ad6271626afdb
gsindri <gsindrih@gmail.com>
2025-09-21 21:10:38 +0000

    Adjust sidebar rail positioning

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -171,13 +171,15 @@ const SidebarRail = React.forwardRef<
       tabIndex={-1}
       onClick={toggleSidebar}
       title="Toggle Sidebar"
-        className={cn(
-          "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all duration-300 ease-in-out after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] hover:after:bg-sidebar-border group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
+      className={cn(
+        "absolute inset-y-0 z-20 hidden w-4 transition-all duration-300 ease-in-out after:absolute after:inset-y-0 after:w-[2px] hover:after:bg-sidebar-border sm:flex",
+        "group-data-[side=left]:-right-4 group-data-[side=left]:after:left-0",
+        "group-data-[side=right]:-left-4 group-data-[side=right]:after:right-0",
         "[[data-side=left]_&]:cursor-w-resize [[data-side=right]_&]:cursor-e-resize",
         "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
-        "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
-        "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
-        "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
+        "group-data-[collapsible=offcanvas]:hover:bg-sidebar",
+        "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2 [[data-side=left][data-collapsible=offcanvas]_&]:after:left-full",
+        "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2 [[data-side=right][data-collapsible=offcanvas]_&]:after:right-full",
         className
       )}
       {...props}
