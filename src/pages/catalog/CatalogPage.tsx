ef18964d935ab88298286054eef3d8b15f5c3b25
Pinky the Genius <gsindrih@gmail.com>
2025-10-10 15:51:07 +0000

    WIP

--- src/pages/catalog/CatalogPage.tsx
+++ src/pages/catalog/CatalogPage.tsx
@@ -1530,7 +1530,7 @@ function FiltersBar({
           </div>
         )}
 
-        <div className={cn(containerClass, "lg:pl-10 xl:pl-22")}>
+        <div className={cn(containerClass, "lg:pl-10 xl:pl-52")}>
           <div className="flex flex-col gap-3 py-3 sm:flex-row sm:items-center sm:justify-between">
             <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:gap-4">
               <div className="relative min-w-0 w-full sm:w-auto sm:flex-none sm:min-w-[400px] sm:max-w-[600px]">
